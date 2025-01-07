2c03658d71a816cab1020c53e22f5cbfeabe236a
scriptertoufiq <scriptertoufiq@gmail.com>
2025-01-07 15:46:29 +0600

    first test case with login session in shopify dashboard

--- tests/dashboard.spec.js
+++ tests/dashboard.spec.js
@@ -2,9 +2,9 @@ const { test, expect } = require('@playwright/test');
 
 test.use({ storageState: 'playwright/.auth/user.json' });
 
-test('Dashboard checking ', async ({ page }) => {
+test('Dashboard checking Of Shopify Store ', async ({ page }) => {
   const cookies = await page.context().cookies();
 
   await page.goto('https://admin.shopify.com/store/toufiq-automation-do-not-delete');
   await expect(page.getByText('Setup guide')).toBeVisible(); // Ensure the authenticated page loads
-});
+});  
