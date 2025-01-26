29a5dc66d105b276a4fb8c22413f2c9cab62aa43
scriptertoufiq <scriptertoufiq@gmail.com>
2025-01-26 09:58:27 +0600

    added settings page redirect

--- tests/image/alt-text.spec.js
+++ tests/image/alt-text.spec.js
@@ -1,13 +1,30 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test.use({ storageState: 'playwright/.auth/user.json' });
+test.use({ storageState: "playwright/.auth/user.json" });
 
-test('Check AI Package Is Subscribed ', async ({ page }) => {
-  const cookies = await page.context().cookies();
+test.describe("Dashboard checking Of Shopify Store ", () => {
+  let dashboardLocator;
 
-  
+  test.beforeEach(async ({ page }) => {
+    await page.goto(
+      "https://admin.shopify.com/store/toufiq-automation-do-not-delete/apps/storeseo-2/image-optimizer"
+    );
+    await page.waitForLoadState("domcontentloaded"); // Better than fixed timeout
+    dashboardLocator = page.frameLocator("iframe[name='app-iframe']");
+  });
 
+  test("Dashboard checking Of Shopify Store ", async ({ page }) => {
+    
+    await page.waitForTimeout(5000);
+    await expect(page.getByText(/Image Optimizer/)).toBeVisible(); // Ensure the authenticated page loads
 
+    let GetButtonTitle = dashboardLocator.getByRole("link", { name: "Manage Settings" });
+    if (await GetButtonTitle.isVisible()) {
+      await GetButtonTitle.click();
+      await page.waitForURL("https://admin.shopify.com/store/toufiq-automation-do-not-delete/apps/storeseo-2/settings/image-optimizer");
+    }
+    
+    
+  });
 
-
-});  
+});
