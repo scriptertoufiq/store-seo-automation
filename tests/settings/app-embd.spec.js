03b68bed6c6d50724e6addc18686a9b3c6aa4516
scriptertoufiq <scriptertoufiq@gmail.com>
2025-05-14 10:52:06 +0600

    app-embed settings page

---
+++ tests/settings/app-embd.spec.js
@@ -0,0 +1,29 @@
+const { test, expect } = require("@playwright/test");
+require('dotenv').config();
+const baseUrl = process.env.BASE_URL;
+
+test.use({ storageState: "playwright/.auth/user.json" });
+
+test.describe("Go to Settings page  ", () => {
+  let dashboardLocator;
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto(
+      `${baseUrl}/apps/storeseo-2/settings/app-embed`
+    );
+    await page.waitForLoadState("domcontentloaded"); // Better than fixed timeout
+    dashboardLocator = page.frameLocator("iframe[name='app-iframe']");
+  });
+
+  test("App embed Settings Page", async ({ page }) => {
+    
+    await page.waitForTimeout(7000);
+    await expect(dashboardLocator.getByText(/Enable App Embed/)).toBeVisible(); // Ensure the authenticated page loads
+
+  });
+
+  
+
+
+
+});
